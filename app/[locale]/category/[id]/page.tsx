1585b95b9032ec971de596d97966ea3bf99fb644
Varvara Borodina <vava.borodina@mail.ru>
2023-09-10 21:51:56 +0300

    fix: separate category page

--- app/[locale]/category/[id]/page.tsx
+++ app/[locale]/category/[id]/page.tsx
@@ -1,73 +1,29 @@
 'use client';
 
-// eslint-disable-next-line simple-import-sort/imports
+import { CategoryPostsBlock } from '@components/blocks/CategoryPostsBlock';
 import { TEXT } from '@constants';
 import categories from '@data/categories.json';
 import posts from '@data/posts.json';
-import tags from '@data/tags.json';
 import { Mulish } from 'next/font/google';
 import { useTranslations } from 'next-intl';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { PageProps } from '@/[locale]/types';
-import { CategoriesBlock } from '@components/blocks/CategoriesBlock';
-import { CategoryPostsBlock } from '@components/blocks/CategoryPostsBlock';
+import { SearchTags } from '@/components/SearchTags';
 import commonStyles from '@/styles/common.module.scss';
-import { Post, Tag } from '@/types';
+import { Post } from '@/types';
 
 import styles from './styled.module.scss';
 
 const mulish = Mulish({ subsets: ['latin'] });
 
-const { NOT_FOUND, BLOG, NO_TAGS, ALL_TAGS, CATEGORIES, SEARCH_TAGS } = TEXT;
-const MAX_TAGS_AMOUNT = 7;
+const { NOT_FOUND, BLOG } = TEXT;
 
 const Category = ({ params: { id } }: PageProps) => {
   const translateNotFound = useTranslations(NOT_FOUND);
   const translate = useTranslations('Categories');
 
-  const translatedTags = tags.map(({ name, id: tagId }) => ({
-    name: translate(name),
-    id: tagId,
-  }));
-
-  const [inputValue, setInputValue] = useState('');
-  const [chosenTags, setChosenTags] = useState<number[]>([]);
-  const [currentTags, setCurrentTags] = useState<Tag[]>(translatedTags.slice(0, MAX_TAGS_AMOUNT));
   const [currentPosts, setCurrentPosts] = useState<Post[]>(posts);
-
-  useEffect(() => {
-    setCurrentPosts(() => {
-      if (chosenTags.length === 0) {
-        return posts;
-      }
-      return posts.filter(({ tags: postTags }) => {
-        return chosenTags.reduce((isChoosen, tag) => isChoosen && postTags.includes(tag), true);
-      });
-    });
-  }, [chosenTags]);
-
-  const handleInputChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = target;
-
-    setInputValue(value);
-
-    if (value === '') {
-      setCurrentTags(translatedTags.slice(0, MAX_TAGS_AMOUNT));
-    } else {
-      setCurrentTags(
-        translatedTags.filter((tag) => tag.name.toLowerCase().includes(value.toLowerCase()))
-      );
-    }
-  };
-
-  const toggleTag = (tagId: number) => () => {
-    if (chosenTags.includes(tagId)) {
-      setChosenTags((prevTags) => prevTags.filter((prevTag) => prevTag !== tagId));
-    } else {
-      setChosenTags((prevTags) => [...prevTags, tagId]);
-    }
-  };
 
   const category = useMemo(
     () => categories.find(({ id: categoryId }) => categoryId === Number(id)),
@@ -77,6 +33,17 @@ const Category = ({ params: { id } }: PageProps) => {
     () => currentPosts.filter((post) => post.category === category?.id),
     [category, currentPosts]
   );
+  const filterPosts = (tags: number[]) => {
+    if (tags.length === 0) {
+      setCurrentPosts(posts);
+    } else {
+      setCurrentPosts(() => {
+        return posts.filter(({ tags: postTags }) => {
+          return tags.reduce((isChoosen, tag) => isChoosen && postTags.includes(tag), true);
+        });
+      });
+    }
+  };
 
   if (!category) {
     return <p className={commonStyles.notFound}>{translateNotFound(NOT_FOUND)}</p>;
@@ -95,28 +62,7 @@ const Category = ({ params: { id } }: PageProps) => {
         <div className={styles.posts}>
           <CategoryPostsBlock posts={categoryPosts} />
         </div>
-        <div className={styles.search}>
-          <input
-            className={styles.input}
-            placeholder={translate(SEARCH_TAGS)}
-            value={inputValue}
-            onChange={handleInputChange}
-          />
-          <h3 className={`${commonStyles.header} ${styles.tagsTitle}`}>{translate(ALL_TAGS)}</h3>
-          <div className={styles.tags}>
-            {!currentTags.length && <p className={styles.description}>{translate(NO_TAGS)}</p>}
-            {currentTags.map((tag) => (
-              <p
-                className={`${styles.tag} ${chosenTags.includes(tag.id) && styles.active}`}
-                key={tag.id}
-                onClick={toggleTag(tag.id)}
-              >
-                {tag.name}
-              </p>
-            ))}
-          </div>
-          <CategoriesBlock title={CATEGORIES} column titleAlign="left" />
-        </div>
+        <SearchTags onTagsChange={filterPosts} />
       </div>
     </>
   );
---
+++ app/components/SearchTags/index.tsx
@@ -0,0 +1,77 @@
+import { CategoriesBlock } from '@components/blocks/CategoriesBlock';
+import { TEXT } from '@constants';
+import tags from '@data/tags.json';
+import { useTranslations } from 'next-intl';
+import React, { useEffect, useState } from 'react';
+
+import commonStyles from '@/styles/common.module.scss';
+import { Tag } from '@/types';
+
+import styles from './styled.module.scss';
+
+const MAX_TAGS_AMOUNT = 7;
+
+const { NO_TAGS, ALL_TAGS, CATEGORIES, SEARCH_TAGS } = TEXT;
+
+export const SearchTags = ({ onTagsChange }: { onTagsChange: (tags: number[]) => void }) => {
+  const translate = useTranslations('Categories');
+  const translatedTags = tags.map(({ name, id: tagId }) => ({
+    name: translate(name),
+    id: tagId,
+  }));
+
+  const [inputValue, setInputValue] = useState('');
+  const [currentTags, setCurrentTags] = useState<Tag[]>(translatedTags.slice(0, MAX_TAGS_AMOUNT));
+  const [chosenTags, setChosenTags] = useState<number[]>([]);
+
+  useEffect(() => {
+    onTagsChange(chosenTags);
+  }, [chosenTags]);
+
+  const handleInputChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = target;
+
+    setInputValue(value);
+
+    if (value === '') {
+      setCurrentTags(translatedTags.slice(0, MAX_TAGS_AMOUNT));
+    } else {
+      setCurrentTags(
+        translatedTags.filter((tag) => tag.name.toLowerCase().includes(value.toLowerCase()))
+      );
+    }
+  };
+
+  const toggleTag = (tagId: number) => () => {
+    if (chosenTags.includes(tagId)) {
+      setChosenTags((prevTags) => prevTags.filter((prevTag) => prevTag !== tagId));
+    } else {
+      setChosenTags((prevTags) => [...prevTags, tagId]);
+    }
+  };
+
+  return (
+    <div className={styles.search}>
+      <input
+        className={styles.input}
+        placeholder={translate(SEARCH_TAGS)}
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <h3 className={`${commonStyles.header} ${styles.tagsTitle}`}>{translate(ALL_TAGS)}</h3>
+      <div className={styles.tags}>
+        {!currentTags.length && <p className={styles.description}>{translate(NO_TAGS)}</p>}
+        {currentTags.map((tag) => (
+          <p
+            className={`${styles.tag} ${chosenTags.includes(tag.id) && styles.active}`}
+            key={tag.id}
+            onClick={toggleTag(tag.id)}
+          >
+            {tag.name}
+          </p>
+        ))}
+      </div>
+      <CategoriesBlock title={CATEGORIES} column titleAlign="left" />
+    </div>
+  );
+};
